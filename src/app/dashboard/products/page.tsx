490ce2bf95693a7cf4da51227f5ba54acb97679c
Noelia Martínez <noeowo384@gmail.com>
2025-03-24 17:24:14 -0400

    created page file for producto route and setting

---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UsersPage() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <div className='bg-amber-900'>01</div>
+    </div>
+  )
+}
