08241363dd6bbc293161547ed375b781b028ab85
Noelia Martínez <noeowo384@gmail.com>
2025-03-24 17:23:13 -0400

    added page unauthorized page

---
+++ src/app/dashboard/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+export default function Unauthorized() {
+    return (
+      <div>
+        <h1>Acceso Denegado</h1>
+        <p>No tienes permisos para ver este contenido.</p>
+      </div>
+    );
+  }
+  
