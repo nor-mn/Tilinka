022b344af8ced24340c3bfca23303cacc98e2d47
Noelia Martínez <noeowo384@gmail.com>
2025-03-29 02:27:14 -0400

    toggle user status

--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useAuth } from "@/context/AuthContext";
-import { db } from "@/firebase/firebase";
-import { collection, doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db, auth } from "@/firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, doc, getDoc, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 type User = {
@@ -11,8 +12,8 @@ type User = {
   email: string;
   role: string;
   company: string;
+  phone_number: string;
   active: boolean;
-  phoneNumber: string;
 };
 
 export const useUsers = () => {
@@ -37,13 +38,32 @@ export const useUsers = () => {
     return () => unsubscribe();
   }, [userData?.id]);
 
-  const deactivateUser = async (userId: any) => {
-    if (userData?.role == "admin") {
-      const userRef = doc(db, "users", userId);
-      await updateDoc(userRef, { active: false });
-    } else {
-      console.error("No tienes permisos para desactivar usuarios");
-    }
+  const addUser = async (email: string, password: string, username: string, phone_number: string, company: string) => {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      //Guardar en firestore
+      await setDoc(doc(db, "users", user.uid), {
+        id: user.uid,
+        username,
+        email: email,
+        role: "user",
+        phone_number,
+        company,
+        active: true
+      })
+      return user;
+  }
+
+  const toggleUserStatus = async (userId: any, currentStatus:boolean) => {
+    if(userData?.role != "admin") return console.log("No tienes permisos");
+    
+      try {
+        const userRef = doc(db, "users", userId);
+        await updateDoc(userRef, {active: !currentStatus});
+        console.log(`Estado ${currentStatus? 'activado':'inactivo'} correntamente.`);
+      } catch (error) {
+        console.log("Error al cambiar el estado del usuario: ", error);
+      }
   };
 
   const editCompany = async (userId: any, newCompany: any) => {
@@ -51,7 +71,7 @@ export const useUsers = () => {
     await updateDoc(userRef, { company: newCompany });
   };
 
-  return { users, deactivateUser, editCompany };
+  return { users, toggleUserStatus, editCompany };
 };
 
 // {
