1f065b5b34e0b3d793286418a3943be9152e9b8b
Noelia Martínez <noeowo384@gmail.com>
2025-03-29 02:27:55 -0400

    added toggle user status

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useUsers } from "@/hooks/useUsers";
+import { BadgeCheck, BadgePlus, BadgeX } from "lucide-react";
 
 const UsersList = () => {
-   const {users} = useUsers();
-   console.log(users,'page Us')
+  const { users, toggleUserStatus } = useUsers();
+  console.log(users, "page Us");
   return (
-    <ul className="space-y-2">
-      <table className="table-auto border-separate border-spacing-2 border border-gray-900">
+    <div className="space-y-2">
+      <table className="table-auto border border-separate border-spacing-2 rounded-xl w-full border-gray-900">
         <thead>
           <tr>
             <th className="p-1 border border-gray-300">Compañia</th>
@@ -19,21 +20,44 @@ const UsersList = () => {
           </tr>
         </thead>
         <tbody>
-          {
-            users.map((user)=>(
-                <tr key={user.id}>
-                    <td className="border border-gray-300 p-1">{user.company}</td>
-                    <td className="border border-gray-300 p-1">{user.username}</td>
-                    <td className="border border-gray-300 p-1">{user?.email}</td>
-                    <td className="border border-gray-300 p-1">{user?.phoneNumber}</td>
-                    <td className="border border-gray-300 p-1">{user.active}</td>
-                    <td className="border border-gray-300 p-1"></td>
-                </tr>
-            ))
-          }
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td className="border border-gray-300 p-1">{user.company}</td>
+              <td className="border border-gray-300 p-1">{user.username}</td>
+              <td className="border border-gray-300 p-1">{user?.email}</td>
+              <td className="border border-gray-300 p-1">
+                {user?.phone_number}
+              </td>
+              <td className="border border-gray-300 p-1">
+                {user.active ? "Activo" : "Inactivo"}
+              </td>
+              <td className="border border-gray-300 p-1">
+                <div className="flex justify-center">
+                  {user.active ? (
+                    <button
+                      className={`rounded-2xl bg-green-400 hover:bg-green-500 cursor-pointer text-gray-900`}
+                      onClick={() => toggleUserStatus(user.id, user.active)}
+                    >
+                      <BadgeCheck />
+                    </button>
+                  ) : (
+                    <button
+                      className={`rounded-2xl bg-red-400 hover:bg-red-500 cursor-pointer text-gray-900`}
+                      onClick={() => toggleUserStatus(user.id, user.active)}
+                    >
+                      <BadgeX />
+                    </button>
+                  )}
+                  <button className="rounded-2xl cursor-pointer bg-palette-003/70 hover:bg-palette-003">
+                    <BadgePlus />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-    </ul>
+    </div>
   );
 };
 
