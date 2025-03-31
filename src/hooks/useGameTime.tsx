64cefabc2dc9c1d5c258a0919fbd940efe511e60
Noelia Martínez <noeowo384@gmail.com>
2025-03-31 02:07:27 -0400

    added hook gameTime: get && add

---
+++ src/hooks/useGameTime.tsx
@@ -0,0 +1,69 @@
+import { useAuth } from '@/context/AuthContext';
+import { db } from '@/firebase/firebase';
+import { addDoc, collection, getDocs, Timestamp } from 'firebase/firestore';
+import { useCallback, useState } from 'react';
+
+type GameTime = {
+    id?: string;
+    date: Timestamp;
+    time: string;
+  };
+
+export const useGameTime = (myAppId:string) => {
+    const { user } = useAuth();
+    const [ gameTime, setGameTime] = useState<GameTime[]>([]);
+    const [ loading, setLoading ] = useState(false);
+    const [ error, setError ] = useState<string | null>(null);
+
+    const getGameTime = useCallback(async () => {
+        if(!user?.uid || !myAppId) return;
+        setLoading(true);
+        setError(null);
+
+        try {
+            const gamePlayTimeRef = collection(db, "users", user.uid, "my-apps", myAppId, "gamePlayTime");
+            const querySnapshot = await getDocs(gamePlayTimeRef);
+
+            const gameTimeList = querySnapshot.docs.map((doc)=>({
+                id: doc.id,
+                date: doc.data().date.toDate(),
+                time: doc.data().time
+            })) as GameTime[];
+
+            setGameTime(gameTimeList);
+            console.log(gameTimeList,'gameTimeList')
+        } catch (error) {
+            setError("Error al obtener el tiempo de juego");
+        } finally {
+            setLoading(false);
+        }
+    }, [user?.uid, myAppId]);
+
+    const addGameTime = async (newGameTime: Omit<GameTime, "id">) => {
+        if(!user.uid || !myAppId) return;
+        setLoading(true);
+
+        try {
+            const gamePlayTimeRef = collection(db, "users", user.uid, "my-apps", myAppId, "gamePlayTime");
+
+            await addDoc(gamePlayTimeRef, {
+                ...newGameTime,
+                date: newGameTime.date,
+                time: newGameTime.time,
+            });
+            setGameTime((prev) => [...prev, newGameTime]);
+        } catch (error) {
+            setError("Error al añadir el tiempo de juego")
+        } finally {
+            setLoading(false);
+        }
+    }
+
+  return {
+    gameTime,
+    loading,
+    error,
+    getGameTime,
+    addGameTime
+  }
+}
